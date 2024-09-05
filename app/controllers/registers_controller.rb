5edb1e0fd9c4b404bb7fa451409c63b35ed0cee0
care-Thepphithak <thepphithak2002@odds.team>
2024-09-06 01:11:28 +0700

    create new Registration record

--- app/controllers/registers_controller.rb
+++ app/controllers/registers_controller.rb
@@ -10,9 +10,30 @@ class RegistersController < ApplicationController
   def edit
   end
 
+  def show
+    @register = Register.find(params[:id])
+  end
+
+  def create
+    @register = Register.new(register_params)
+    respond_to do |format|
+      if @register.save
+        format.html { redirect_to registers_url, notice: "Registration complete! Your details are now saved." }
+        format.json { render :index, status: :created, location: @register }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @register.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   def update
   end
 
   def destory
   end
+end
+
+def register_params
+  params.require(:register).permit(:first_name, :last_name, :b_date, :gender, :email, :phone_num, :subject)
 end
