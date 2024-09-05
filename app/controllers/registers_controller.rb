360029dab58eab917fd8311a0e7f903841e423f7
care-Thepphithak <thepphithak2002@odds.team>
2024-09-06 01:56:57 +0700

    update a Registration record

--- app/controllers/registers_controller.rb
+++ app/controllers/registers_controller.rb
@@ -28,6 +28,16 @@ class RegistersController < ApplicationController
   end
 
   def update
+    @register = Register.find(params[:id])
+    respond_to do |format|
+      if @register.update(register_params)
+        format.html { redirect_to registers_url, notice: "#{@register.first_name} #{@register.last_name}'s Registration details is updated." }
+        format.json { render :index, status: :ok, location: @register }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @register.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
